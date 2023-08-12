a442b2cb8773c7ea726911e4d418379b3e41d01a
Jing <42014615+jing332@users.noreply.github.com>
2023-08-12 18:40:24 +0800

    action cache

--- alist-lib/scripts/install_aar.sh
+++ alist-lib/scripts/install_aar.sh
@@ -4,4 +4,4 @@ else
   gomobile bind -ldflags "-s -w" -v -target="android/arm,android/arm64" -androidapi 21 "github.com/alist-org/alist/v3/alistlib"
 fi
 
-cp -f *.aar ../app/libs
+cp -f *.aar ../../app/libs
--- app/src/main/java/com/github/jing332/alistandroid/service/AlistService.kt
+++ app/src/main/java/com/github/jing332/alistandroid/service/AlistService.kt
@@ -1,20 +1,21 @@
 package com.github.jing332.alistandroid.service
 
+import alistlib.Alistlib
+import alistlib.LogCallback
 import android.app.Service
 import android.content.Intent
 import android.os.IBinder
-import androidlib.Androidlib
-import androidlib.LogCallback
+import android.util.Log
 import com.github.jing332.alistandroid.util.ToastUtils.longToast
 
 class AlistService : Service() {
     companion object {
+        const val TAG = "AlistService"
         const val ACTION_SHUTDOWN =
             "com.github.jing332.alistandroid.service.AlistService.ACTION_SHUTDOWN"
 
         init {
-            Androidlib.init(object : LogCallback {
-            })
+//            Alistlib.init { level, msg -> Log.i(TAG, "level=${level}, msg={$msg}") }
         }
     }
 
@@ -22,13 +23,13 @@ class AlistService : Service() {
 
     override fun onCreate() {
         super.onCreate()
-        Androidlib.start()
+        Alistlib.start()
     }
 
     override fun onStartCommand(intent: Intent, flags: Int, startId: Int): Int {
         if (intent.action == ACTION_SHUTDOWN) {
             runCatching {
-                Androidlib.shutdown(5000)
+                Alistlib.shutdown(5000)
             }.onFailure {
                 longToast("Shutdown failed: ${it.message}")
             }
