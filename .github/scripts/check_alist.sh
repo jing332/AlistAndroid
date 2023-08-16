33d603cf92f368b489a92384cbc06e5e079ed302
Jing <42014615+jing332@users.noreply.github.com>
2023-08-16 15:44:06 +0800

    feat: Action 每小时自动检查AList版本

--- .github/scripts/check_alist.sh
+++ .github/scripts/check_alist.sh
@@ -7,22 +7,24 @@ function to_int() {
 LATEST_VER=$(curl -s https://api.github.com/repos/alist-org/alist/releases/latest | grep -o '"tag_name": ".*"' | cut -d'"' -f4)
 LATEST_VER_INT=$(to_int $LATEST_VER)
 echo "Latest AList version $LATEST_VER ${LATEST_VER_INT}"
+echo "alist_version=$LATEST_VER" >> $GITHUB_ENV
 
 # VERSION_FILE="$GITHUB_WORKSPACE/alist_version.txt"
 
 VER=$(cat $VERSION_FILE)
 if [ -z $VER ]; then
-  VER="0"
+  VER="v3.25.1"
 fi
 
 VER_INT=$(to_int $VER)
 
 echo "Current AList version: $VER ${VER_INT}"
 
+
 if [ $VER_INT -ge $LATEST_VER_INT ]; then
     echo "Current >= Latest"
-    exit 0
+    echo "alist_update=false" >> $GITHUB_ENV
 else
     echo "Current < Latest"
-    exit 1
+    echo "alist_update=true" >> $GITHUB_ENV
 fi
